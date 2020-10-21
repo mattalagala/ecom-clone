10728707126088db8e0409ba1e0abec9f699b073
matt15y <matt15y@gmail.com>
2020-10-21 16:25:22 -0500

    Initial Commit

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Header() {
+	return (
+		<div className='header'>
+			<img
+				className='header__logo'
+				src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+			/>
+			<div className='header__search'>
+				<input className='header__searchInput' type='text' />
+				<SearchIcon class='header__searchIcon' />
+			</div>
+			<div className='header__nav'>
+				<div className='header__option'>
+					<span class='header__optionLineOne'>Hello Guest</span>
+					<span class='header__optionLineOne'>Sign In</span>
+				</div>
+
+				<div className='header__option'>
+					<span class='header__optionLineOne'>Returns</span>
+					<span class='header__optionLineOne'>& Orders</span>
+				</div>
+
+				<div className='header__option'>
+					<span class='header__optionLineOne'>Your</span>
+					<span class='header__optionLineOne'>Prime</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
